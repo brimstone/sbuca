23d171527de3b00e4c26511bac60d22bcc818765
Matt <brimstone@the.narro.ws>
2015-09-08 17:53:20 -0400

    A little `go fmt` magic.

--- ca/cert_store.go
+++ ca/cert_store.go
@@ -1,67 +1,66 @@
 package ca
 
 import (
-  "github.com/gophergala/sbuca/pkix"
-  "strconv"
-  "io/ioutil"
+	"github.com/gophergala/sbuca/pkix"
+	"io/ioutil"
+	"strconv"
 )
 
 type CertStore struct {
-
-  RootDir string
-
+	RootDir string
 }
 
-func NewCertStore(rootDir string) *CertStore{
+func NewCertStore(rootDir string) *CertStore {
 
-  store := &CertStore {
-    RootDir: rootDir,
-  }
+	store := &CertStore{
+		RootDir: rootDir,
+	}
 
-  return store
+	return store
 }
 
 func (store *CertStore) Get(id int64) (*pkix.Certificate, error) {
-  // FIXME
-  // currently using serialnumber as id, should change to something which can be
-  // mapped to (host, sn) pair
-  filename := strconv.Itoa(int(id)) + ".crt"
+	// FIXME
+	// currently using serialnumber as id, should change to something which can be
+	// mapped to (host, sn) pair
+	filename := strconv.Itoa(int(id)) + ".crt"
 
-  cert, err := pkix.NewCertificateFromPEMFile(store.RootDir + "/" + filename)
-  if err != nil {
-    return nil, err
-  }
+	cert, err := pkix.NewCertificateFromPEMFile(store.RootDir + "/" + filename)
+	if err != nil {
+		return nil, err
+	}
 
-  return cert, nil
+	return cert, nil
 }
 
 func (store *CertStore) Put(id int64, cert *pkix.Certificate) error {
 
-  pemBytes, err := cert.ToPEM()
-  if err != nil {
-    return err
-  }
-  filename := strconv.Itoa(int(id)) + ".crt"
-  err = ioutil.WriteFile(store.RootDir + "/" + filename, pemBytes, 0400)
-  if err != nil {
-    return err
-  }
+	pemBytes, err := cert.ToPEM()
+	if err != nil {
+		return err
+	}
+	filename := strconv.Itoa(int(id)) + ".crt"
+	err = ioutil.WriteFile(store.RootDir+"/"+filename, pemBytes, 0400)
+	if err != nil {
+		return err
+	}
 
-  return nil
+	return nil
 }
 
 func (store *CertStore) GetAllNames() ([]string, error) {
 
-  files, err := ioutil.ReadDir(store.RootDir + "/")
-  if err != nil {
-    return nil, err
-  }
-  names := make([]string, len(files))
-  for _, f := range files {
-    names = append(names, f.Name())
-  }
+	files, err := ioutil.ReadDir(store.RootDir + "/")
+	if err != nil {
+		return nil, err
+	}
+	names := make([]string, len(files))
+	for _, f := range files {
+		names = append(names, f.Name())
+	}
 
-  return names, nil
+	return names, nil
 
 }
-  // should limit to 100 FIXME
+
+// should limit to 100 FIXME
--- pkix/csr.go
+++ pkix/csr.go
@@ -1,98 +1,93 @@
 package pkix
 
 import (
-
-  "errors"
-  "crypto/x509"
-  "io/ioutil"
-  "encoding/pem"
-  "crypto/rand"
+	"crypto/rand"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"io/ioutil"
 )
 
 type CertificateRequest struct {
+	DerBytes []byte
 
-  DerBytes []byte
-
-  Csr *x509.CertificateRequest
-
+	Csr *x509.CertificateRequest
 }
 
 func NewCertificateRequest(key *Key) (*CertificateRequest, error) {
-  template := &x509.CertificateRequest{
-    Subject: GenSubject(""), //FIXME
-    //Attributes:
-    //SignatureAlgorithm,
-    //Extensions:
-    DNSNames: []string{},
-    //EmailAddress:
-    //IPAddresses:
-  }
+	template := &x509.CertificateRequest{
+		Subject: GenSubject(""), //FIXME
+		//Attributes:
+		//SignatureAlgorithm,
+		//Extensions:
+		DNSNames: []string{},
+		//EmailAddress:
+		//IPAddresses:
+	}
 
-  derBytes, err := x509.CreateCertificateRequest(rand.Reader, template, key.PrivateKey)
-  if err != nil {
-    return nil, err
-  }
-  csr, err := NewCertificateRequestFromDER(derBytes)
-  if err != nil {
-    return nil, err
-  }
+	derBytes, err := x509.CreateCertificateRequest(rand.Reader, template, key.PrivateKey)
+	if err != nil {
+		return nil, err
+	}
+	csr, err := NewCertificateRequestFromDER(derBytes)
+	if err != nil {
+		return nil, err
+	}
 
-  return csr, nil
+	return csr, nil
 }
 
 func NewCertificateRequestFromDER(derBytes []byte) (*CertificateRequest, error) {
 
-  csr, err := x509.ParseCertificateRequest(derBytes)
-  if err != nil {
-    return nil, err
-  }
+	csr, err := x509.ParseCertificateRequest(derBytes)
+	if err != nil {
+		return nil, err
+	}
 
-  certificateRequest := &CertificateRequest{
-    DerBytes: derBytes,
-    Csr: csr,
-  }
+	certificateRequest := &CertificateRequest{
+		DerBytes: derBytes,
+		Csr:      csr,
+	}
 
-  return certificateRequest, nil
+	return certificateRequest, nil
 }
 func NewCertificateRequestFromPEM(pemBytes []byte) (*CertificateRequest, error) {
 
-  pemBlock, _ := pem.Decode(pemBytes)
-  if pemBlock == nil {
-    return nil, errors.New("PEM decode failed")
-  }
+	pemBlock, _ := pem.Decode(pemBytes)
+	if pemBlock == nil {
+		return nil, errors.New("PEM decode failed")
+	}
 
-  csr, err := x509.ParseCertificateRequest(pemBlock.Bytes)
-  if err != nil {
-    return nil, err
-  }
+	csr, err := x509.ParseCertificateRequest(pemBlock.Bytes)
+	if err != nil {
+		return nil, err
+	}
 
-  certificateRequest := &CertificateRequest{
-    DerBytes: pemBlock.Bytes,
-    Csr: csr,
-  }
+	certificateRequest := &CertificateRequest{
+		DerBytes: pemBlock.Bytes,
+		Csr:      csr,
+	}
 
-  return certificateRequest, nil
+	return certificateRequest, nil
 }
 func NewCertificateRequestFromPEMFile(filename string) (*CertificateRequest, error) {
 
-  data, err := ioutil.ReadFile(filename)
-  if err != nil {
-    return nil, err
-  }
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
 
-  return NewCertificateRequestFromPEM(data)
+	return NewCertificateRequestFromPEM(data)
 }
 
 func (csr *CertificateRequest) ToPEM() ([]byte, error) {
 
-  pemBlock := &pem.Block{
-    Type: "CERTIFICATE REQUEST",
-    Bytes: csr.DerBytes,
-  }
+	pemBlock := &pem.Block{
+		Type:  "CERTIFICATE REQUEST",
+		Bytes: csr.DerBytes,
+	}
 
-  pemBytes := pem.EncodeToMemory(pemBlock)
+	pemBytes := pem.EncodeToMemory(pemBlock)
 
-  return pemBytes, nil
+	return pemBytes, nil
 }
-
-
