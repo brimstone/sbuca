de85e3caef3d2da03c40027a4271ef5ac5dfa249
Matt <brimstone@the.narro.ws>
2015-09-15 21:17:32 -0400

    Adding admin authorization function

--- server/authorize.go
+++ server/authorize.go
@@ -7,3 +7,9 @@ func authorizeSigning(res http.ResponseWriter, req *http.Request) {
 		res.WriteHeader(http.StatusUnauthorized)
 	}
 }
+
+func authorizeAdmin(res http.ResponseWriter, req *http.Request) {
+	if req.Header.Get("X-API-KEY") != config["admin-token"] {
+		res.WriteHeader(http.StatusUnauthorized)
+	}
+}
