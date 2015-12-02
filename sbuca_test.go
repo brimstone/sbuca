67a37664d6fbbd5a7c6b2eb85d99b8d68ee7cd2b
Matt <brimstone@the.narro.ws>
2015-12-01 20:24:54 -0500

    Adding test for sbuca main

---
+++ sbuca_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func SetupTest() string {
+	dir, err := ioutil.TempDir(os.TempDir(), "sbuca-")
+
+	if err != nil {
+		panic(err)
+	}
+	return dir
+}
+
+func TearDown(dir string) {
+	os.RemoveAll(dir)
+}
+
+func Test_main(t *testing.T) {
+	defer TearDown(SetupTest())
+	oldArgs := os.Args
+	defer func() { os.Args = oldArgs }()
+
+	os.Args = []string{"sbuca"}
+	main()
+}
+
+func Test_main_genkey(t *testing.T) {
+	defer TearDown(SetupTest())
+	oldArgs := os.Args
+	defer func() { os.Args = oldArgs }()
+
+	os.Args = []string{"sbuca", "oneshot"}
+	main()
+}
